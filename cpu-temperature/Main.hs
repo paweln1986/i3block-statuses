eb1eb93d7dc1afbce0c9332d4cbf4c693f68e3fb
Pawel Nosal <p.nosal1986@gmail.com>
2019-12-02 23:54:52 +0100

    fix fan traversl

--- cpu-temperature/Main.hs
+++ cpu-temperature/Main.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import           BlockDetails
-import qualified Data.Aeson          as Aeson
+import           Control.Lens    hiding (strict)
+import qualified Data.Aeson      as Aeson
 import           Data.Aeson.Lens
-import           Control.Lens       hiding (strict)
+import qualified Data.Text       as Text
 import           Relude
-import           Turtle hiding (view)
-import qualified Data.Text as Text
+import           Turtle          hiding (view)
 
 fanIcon :: Text
 fanIcon = "\63587"
@@ -21,12 +22,14 @@ temperatureIcon = "\62154"
 main :: IO ()
 main =
   sh $ do
-    a <- cpuStats'
-    let fanDetails = a ^.. key "dell_smm-virtual-0" . _Object . traverse . _Object . traverse . _Integer
-    let cpuDetails = a ^? key "coretemp-isa-0000" . key "Package id 0" . key "temp1_input" . _Integer
-    let fan = Text.intercalate " " $ fmap (\val -> fanIcon <> " "<> show val) fanDetails
-    let cpu = foldl' (\_ y -> temperatureIcon <> ""<>show y <> degreeIcon) (temperatureIcon <>"N/A") cpuDetails
+    sensorsResponse <- cpuStats'
+    let fanDetails = sensorsResponse ^.. _Object . traverse . fanKeyNames . members . _Integer
+    let cpuDetails = sensorsResponse ^? _Object . traverse . key "Package id 0" . key "temp1_input" . _Integer
+    let fan = Text.intercalate " " $ fmap (\val -> fanIcon <> " " <> show val) fanDetails
+    let cpu = foldl' (\_ y -> temperatureIcon <> "" <> show y <> degreeIcon) (temperatureIcon <> "N/A") cpuDetails
     putLBSLn $ Aeson.encode $ defaultBlockDetails & fullText .~ (" " <> fan <> " " <> cpu <> " ")
+  where
+    fanKeyNames = mconcat $ fmap (\val -> key $ "fan" <> show val) [0 :: Int .. 9]
 
 cpuStats' :: Shell Text
 cpuStats' = strict $ inshell "sensors -j" empty
